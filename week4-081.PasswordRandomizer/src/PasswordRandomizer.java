c6cd1586df4934b8ef45111f7c72bfe8097a731d
PiotrKrolwsb <piotr.krol@protonmail.com>
2016-03-12 19:43:36 +0100

    100%

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,25 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private Random random;
+    private int passwordLength;
 
-    public PasswordRandomizer(int length) {
-        // Initialize the variable
+    public PasswordRandomizer(int length) 
+    {
+        this.passwordLength = length;
+        random = new Random();
     }
 
     public String createPassword() {
         // write code that returns a randomized password
-        return "";
+            int i = 0;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String newPassword = "";
+        while (i < this.passwordLength) 
+        {
+            newPassword = newPassword + alphabet.charAt(this.random.nextInt(26));
+            i++;
+        }
+        return newPassword;
     }
 }
