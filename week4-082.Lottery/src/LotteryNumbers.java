8118285b8ed8b0419f5755d5321e627b691b7551
PiotrKrolwsb <piotr.krol@protonmail.com>
2016-03-12 19:59:38 +0100

    100%

--- week4-082.Lottery/src/LotteryNumbers.java
+++ week4-082.Lottery/src/LotteryNumbers.java
@@ -1,26 +1,41 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class LotteryNumbers {
+public class LotteryNumbers 
+{
     private ArrayList<Integer> numbers;
+    private Random random = new Random();
 
-    public LotteryNumbers() {
+    public LotteryNumbers() 
+    {
         // Draw numbers as LotteryNumbers is created
         this.drawNumbers();
     }
-
-    public ArrayList<Integer> numbers() {
+    public ArrayList<Integer> numbers() 
+    {
         return this.numbers;
     }
-
-    public void drawNumbers() {
+    public void drawNumbers() 
+    {
         // We'll format a list for the numbers
         this.numbers = new ArrayList<Integer>();
         // Write the number drawing here using the method containsNumber()
+        int i = 0;
+        int newNum = 0;
+        while (i < 7) 
+        {
+            newNum = this.random.nextInt(39) + 1;
+            if (!containsNumber(newNum)) 
+            {
+                this.numbers.add(newNum);
+                i++;
+            }
+        }
     }
 
-    public boolean containsNumber(int number) {
+    public boolean containsNumber(int number) 
+    {
         // Test here if the number is already in the drawn numbers
-        return true;
+        return this.numbers.contains(number);
     }
 }
