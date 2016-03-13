2c3983260c0d86e94bbed95d892ab7fef87755e0
PiotrKrolwsb <piotr.krol@protonmail.com>
2016-03-13 17:51:07 +0100

    13.03.2016

--- week4-083.HangmanLogic/src/HangmanLogic.java
+++ week4-083.HangmanLogic/src/HangmanLogic.java
@@ -5,41 +5,66 @@ public class HangmanLogic {
     private String guessedLetters;
     private int numberOfFaults;
 
-    public HangmanLogic(String word) {
+    public HangmanLogic(String word) 
+    {
         this.word = word.toUpperCase();
         this.guessedLetters = "";
         this.numberOfFaults = 0;
     }
-
-    public int numberOfFaults() {
+    public int numberOfFaults() 
+    {
         return this.numberOfFaults;
     }
-
-    public String guessedLetters() {
+    public String guessedLetters() 
+    {
         return this.guessedLetters;
     }
-
-    public int losingFaultAmount() {
+    public int losingFaultAmount() 
+    {
         return 12;
     }
-
-    public void guessLetter(String letter) {
-        // program here the functionality for making a guess
-
+    public void guessLetter(String letter) 
+    {
+          //if ( guessedLetters.contains(letter)) {}
+          //else { guessedLetters+=letter;}
+         // if (this.word.contains(letter)) {}
+         // else  this.numberOfFaults++;
         // if the letter has already been guessed, nothing happens
-
         // it the word does not contains the guessed letter, number of faults increase
         // the letter is added among the already guessed letters
+        if (this.guessedLetters.contains(letter)) {
+            return;
+        }
+        // if the word does not contains the guessed letter, number of faults increase
+        // the letter is added among the already guessed letters
+        if (!this.word.contains(letter)) {
+            this.numberOfFaults++;
+        }
+        this.guessedLetters = this.guessedLetters + letter;
     }
-
-    public String hiddenWord() {
-        // program here the functionality for building the hidden word
-
+    public String hiddenWord() 
+    {
+        // program here the functionality for building the hidden word     
         // create the hidden word by interating through this.word letter by letter
         // if the letter in turn is within the guessed words, put it in to the hidden word
         // if the letter is not among guessed, replace it with _ in the hidden word 
-
         // return the hidden word at the end
-        return "";
+        String hidden = "";
+        int i =0;
+        String s = "";
+        while (i < this.word.length())
+        {
+            s = "" + this.word.charAt(i);
+            if (this.guessedLetters.contains(s))
+            {
+                hidden = hidden +s;
+            }
+            else
+            {
+                hidden = hidden + "_";
+            }
+            i++;
+        }
+        return hidden;
     }
 }
